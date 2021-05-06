13f065f7ad3538335e2c4191b79ec15d8e53c272
Vinicius Lopes <cafeh2002@gmail.com>
2021-05-06 16:18:35 -0300

    calculadora esqueleto

---
+++ com/calculadora/calculadora.java
@@ -0,0 +1,35 @@
+// ANOTAÇOES
+// 1- SELEÇÃO DE OPERAÇÃO 2- AQUISIÇÃO DE VALOR DAS VARIAVEIS DA OPERAÇÕES 3- IMPRESSÃO DE RESULTADOS
+package com.calculadora;
+
+public class calculadora{
+     public static void main(String[] args) {
+        System.out.println("INSIRA O TIPO DE OPERAÇÃO:1-adição,2-subtração,3-multiplicação,4-divisão");
+        int operação = 0;
+        switch(operação) {
+            case 1:
+            System.out.println("Insira numeros da operação");
+
+            break;
+            case 2:
+            System.out.println("Insira numeros da operação");
+
+            break;
+            case 3:
+            System.out.println("Insira numeros da operação");
+
+            break;
+            case 4:
+            System.out.println("Insira numeros da operação");
+
+            break;
+
+
+
+
+
+
+        }
+    
+}
+}
---
+++ com/calculadora/system.java
@@ -0,0 +1,8 @@
+package com.calculadora;
+
+public class system {
+
+    public static Object out;
+    public static Object in;
+
+}
